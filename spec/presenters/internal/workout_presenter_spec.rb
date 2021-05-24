92dc3b001569151a9623e732fea9976cd3dfcb49
wesley wong <wesley@freeletics.com>
2021-05-24 18:14:28 +0200

    add presenter spec

---
+++ spec/presenters/internal/workout_presenter_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe Internal::WorkoutPresenter do
+  describe "#to_h" do
+    it "returns has of workout, with total duration and correctly ordered" do
+      pushups = create(:exercise, slug: "pushups")
+      situps = create(:exercise, slug: "situps")
+      workout = create(:workout, slug: "burdell", workout_exercises: [])
+
+      create(:workout_exercise, workout: workout, exercise: pushups, order: 2, duration: 15)
+      create(:workout_exercise, workout: workout, exercise: situps, order: 1, duration: 25)
+
+      expected = {
+        exercises: [
+          { slug: "situps", order: 1, duration: 25 },
+          { slug: "pushups", order: 2, duration: 15 }
+        ],
+        total_duration: 40,
+        state: "draft"
+      }
+
+      expect(described_class.new(workout.reload).to_h).to eq(expected)
+    end
+  end
+end
